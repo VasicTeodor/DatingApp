929cb450a18c5810b4c2a5de5c39ff5d3a7a2fab
Teodor Vasić <teovasic@gmail.com>
2019-05-22 00:08:00 +0200

    Finished edit user component.

--- DatingApp/DatingApp/DatingApp.Api/Helpers/AutoMapperProfiles.cs
+++ DatingApp/DatingApp/DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatingApp.Api.Helpers
                     opt.MapFrom(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
