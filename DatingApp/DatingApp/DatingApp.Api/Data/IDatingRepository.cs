9a9d510b980b4b642910b2f12ee1aa62922bdd0c
Teodor Vasić <teovasic@gmail.com>
2019-05-29 02:11:43 +0200

    Added paging, filtering and sorting of matches.

--- DatingApp/DatingApp/DatingApp.Api/Data/IDatingRepository.cs
+++ DatingApp/DatingApp/DatingApp.Api/Data/IDatingRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DatingApp.Api.Helpers;
 using DatingApp.Api.Models;
 
 namespace DatingApp.Api.Data
@@ -10,7 +11,7 @@ namespace DatingApp.Api.Data
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
         Task<bool> SaveAll();
-        Task<IEnumerable<User>> GetUsers();
+        Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
