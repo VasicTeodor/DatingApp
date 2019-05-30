45910dbdfb99916ba5c423c72e3a7503a8674378
Teodor Vasić <teovasic@gmail.com>
2019-05-31 00:51:03 +0200

    Added like user function to api and spa.

--- DatingApp/DatingApp/DatingApp.Api/Models/User.cs
+++ DatingApp/DatingApp/DatingApp.Api/Models/User.cs
@@ -20,5 +20,7 @@ namespace DatingApp.Api.Models
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<Like> Likers { get; set; }
+        public ICollection<Like> Likees { get; set; }
     }
 }
